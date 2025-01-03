064d540c6424cf5cdbdbd383646f31fa3273ebbd
Steven Maude <git@stevenmaude.co.uk>
2025-01-03 17:56:24 +0000

    Remove right-to-left reverse word ordering
    Restores previous behaviour of Poppler, which pdf2msgpack currently
    relies on for consistency of output with previous versions of
    pdf2msgpack.

--- poppler/TextOutputDev.cc
+++ poppler/TextOutputDev.cc
@@ -4268,11 +4268,6 @@ void TextSelectionDumper::startLine()
 void TextSelectionDumper::finishLine()
 {
     if (!words.empty()) {
-        // Reverse word order for RTL text. Fixes #53 for glib backend (Evince)
-        if (!page->primaryLR) {
-            std::ranges::reverse(words);
-        }
-
         lines.push_back(std::move(words));
     }
     words.clear();
